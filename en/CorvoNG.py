f0e0826dc7ced8cc0a2c591eac500c660ddceeda
f_dutratineesilva <felipe.dutratineesilva@mindgeek.com>
2022-03-26 12:43:00 -0400

    First commit ?

---
+++ en/CorvoNG.py
@@ -0,0 +1,176 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	Corvophraseur New Generation
+	-----------------------------
+	copyright 2010, Pierre-Alain Dorange
+	
+	Adapted from the AppleScript CorvoX (for Adium/Mac) from Aurelien
+		http://www.adiumxtras.com/index.php?a=xtras&xtra_id=2646
+	Original concept : Luc 'Skywalker' Heinrich
+		Corvophraseur 2 (for MacOS 9)
+
+	BSD Licence
+"""
+
+__author__="Pierre-Alain Dorange (pdorange@mac.com)"
+__translator_ = "tkanos"
+__date__="6 avril 2010"
+__copyright__="Copyright 2010, Pierre-Alain Dorange"
+__license__="BSD"
+__version__="0.1a"
+
+import random
+
+verbs = ( 	("access",2), ("speed up",2), ("adjust",2), ("upgrade",2), ("amplify",2), 
+			("analyse",2), ("cancel",2), ("nullify",2), ("override",2), ("boil",2), ("calcify",2), 
+			("compress",2), ("create",2), ("form",2), ("decelerate",2), ("decode",2), ("decipher",2), ("decrypt",2), 
+			("destabilize",2), ("detect",2), ("decrease",2), ("replicate",2), ("encode",2), 
+			("encrypt",2), ("shatter",2), ("smash",2), ("induce",2), ("inhibit",2), ("disable",2), ("reverse",2),
+			("ionize",2), ("module",2), ("neutralize",2), ("optimize",2), ("phase",2), 
+			("polarize",2), ("purify",2), ("rearrange",2), ("recombine",2), ("recreate",2), 
+			("redirect",2), ("relay",2), ("take over",2), ("scan",2), ("report",2), ("stabilize",2), ("normalize",2), 
+			("overload",2), ("transform",2), ("process",2), ("make",2), ("hole",2), ("zombify",2),  ("helm",2), ("engage",2)
+		) 
+adjs = (	("short range", "short range", 0), ("wide range", "wide range", 0), 
+			("high speed", "high speed", 0), ("adaptive", "adaptive", 2), ("alternative", "alternative", 2), 
+			("honeycombed", "honeycombed", 2), ("artificial", "artificial", 2), ("atomic", "atomic", 2), ("nuclear", "nuclear", 2), 
+			("whaled", "whaled", 2), ("ballistic", "ballistic", 2), ("low speed", "low speed", 2), ("decentralised", "decentralised", 2), 
+			("binary", "binary", 2), ("duple", "duple", 2), ("calorific", "calorific", 2), 
+			("carpal", "carpal", 2), ("centric", "centric", 2), ("centered", "centered", 2), ("compressed", "compressed", 2), 
+			("connotative", "connotative", 2), ("corporeal", "corporeal", 2), ("cytherian", "cytherian", 2), 
+			("dimensional", "dimensional", 2), ("directional", "directional", 2), ("directed", "directed", 2), 
+			("dynamic", "dynamic", 2), ("encrypted", "encrypted", 2), ("genetic", "genetic", 2), 
+			("gildoic", "gildoic", 2), ("gravity", "gravity", 2), ("gravitational", "gravitational", 2), 
+			("holographic", "holographic", 2), ("unstable", "unstable", 2), ("interstellar", "interstellar", 2), 
+			("ionized", "ionized", 2), ("linear", "linear", 2), ("localized", "localized", 2), ("magnetic", "magnetic", 2), 
+			("mechanical", "mechanical", 2), ("microscopic", "microscopic", 2), ("modular", "modular", 2), 
+			("molecular", "molecular", 2), ("moncturian", "moncturian", 2), ("navigational", "navigational", 2), 
+			("oblative", "oblative", 2), ("ossiphazole", "ossiphazole", 2), ("parabolic", "parabolic", 2), 
+			("parallel", "parallel", 2), ("phased", "phased", 2), ("phasic", "phasic", 2), ("plutonizing", "plutonizing", 2), 
+			("plutonized", "plutonized", 2), ("pouring", "pouring", 2), ("quantic", "quantic", 2), ("quantum", "quantum", 2), 
+			("cooled", "cooled", 2), ("replicative", "replicative", 2), ("resistant", "resistant", 2), 
+			("resonant", "resonant", 2), ("spatial", "spatial", 2), ("spinoidal", "spinoidal", 2), ("static", "static", 2), 
+			("stellar", "stellar", 2), ("temporal", "temporal", 2), ("trigloidal", "trigloidal", 2), ("valved", "valved",2), 
+			("swift","swift",2), ("vibratile","vibratile",2), ("magnifying","magnifying",2)
+		)
+nouns = ( 	("magnet",1,2), ("alignment",1,2), ("amplifier",1,2), ("anihilator",1,2), ("capsule",2,2), 
+			("assimilator",1,2), ("beacon",2,2), ("tag",2,2), ("shield",1,2), 
+			("calcificator",1,2), ("capacity",2,2), ("capilectomy",2,2), ("causality",2,2), ("room",2,2), 
+			("field",1,2), ("force field",1,2), ("heart",1,2), ("pipe",1,2), ("configuration",2,2), 
+			("continuum",1,2), ("converter",1,2), ("corridor",1,2), ("rift",2,2), ("cristal",1,2), 
+			("cyberment",1,2), ("deflector",1,2), ("disintegrator",1,2), ("detonator",1,2), ("trigger",1,2), 
+			("diagnostic",1,2), ("disruptor",1,2), ("distortion",2,2), ("echo",1,2), ("efficiency",2,2), 
+			("emission",2,2), ("transmission",2,2), ("containment",1,2), ("energy",2,2), ("entity",2,2), ("filament",1,2), 
+			("filter",1,2), ("flow",1,2), ("flood",1,2), ("stream",1,2), ("flux",1,2), ("force",2,2), ("forming",2,2), ("fragment",1,2), ("chip",1,2), 
+			("motherboard",1,2),  ("fragmenticle",1,2), ("frequency",2,2), ("gain",1,2), ("generator",1,2), ("jet",1,2),  ("spray",1,2), 
+			("glomerulus",2,2), ("goniotron",1,2), ("graviton",1,2), ("grid",2,2), ("hologram",1,2), 
+			("impulsion",2,2), ("incursion",2,2), ("inductor",1,2), ("inertia",1,2), ("inhibitor",1,2), 
+			("plaxmol",1,2), ("matter",2,2), ("matrix",2,2), ("mecanism",1,2), ("bomb",2,2), 
+			("mitochondria",2,2), ("engine",1,2), ("motor",1,2), ("moulinotron",1,2), ("reelmill",1,2), ("multiplexer",1,2), ("muxer",1,2), ("pod",2,2), 
+			("core",1,2), ("nucleur",1,2), ("cloud",1,2), ("computer",1,2), ("parallax",1,2), 
+			("particle",2,2), ("plasma",1,2), ("plutonator",1,2), ("bearer",2,2), ("impulse",2,2), 
+			("radiation",2,2), ("beam",1,2), ("booster",1,2), ("relay",1,2), ("grinder",1,2), 
+			("replicator",1,2), ("replicatant",1,2),("replication",2,2), ("network",1,2), ("resonator",1,2), ("spring",1,2), 
+			("rotation",2,2), ("schism",1,2), ("sequence",2,2), ("signal",1,2), ("signature",2,2), 
+			("singularity",2,2), ("probe",2,2), ("sensor",2,2), ("sponsor",1,2), ("sustenance",2,2), 
+			("syntagm",1,2), ("tachyon",1,2), ("transistor",1,2), ("translator",1,2), ("transporter",1,2), 
+			("trigloid",1,2), ("trophoblast",1,2), ("tropism",1,2), ("tube",1,2), ("conduit",1,2), ("tunnel",1,2), 
+			("turbulence",1,2), ("wave",2,2), ("vibration",2,2), ("vacuum",1,2), ("void",1,2), ("vortex",1,2)
+		)
+prefix = ( 	"aero","ana","anti","auto","bi","bulbo","capillo","crypto","cyber", "extra","hepta","hetero",
+			"homo","meta","micro","morpho","morvo","multi","neo","non","nucleo","octo","penta",
+			"poly","proto","pseudo","quadri","rétro","servo","spiro","sub","sur","thermo","theta",
+			"trans","tri","turbo" )
+gabarits = ("If we can [v] the [a] [n] , we could [v] the [a] [n] and [v] the [n]", 
+			"Captain, I can't [v] the [n] because the [a] [n] is about [d] the [a] [n]  !",
+			"Captain, we have to [v] the [a] [n] into the [a] to avoid a [a] [n]!",
+			"[v] the [a] [n] is illogical, because the [a] [n] is going to [v] the [a] [n].", 
+			"It's possible that the [a] [n] could [v] the [a] [n], but only if we could [v] the [a] [n] and [v] the [a] [n] !", 
+			"Don't panic ! [v] the [a] [n] so we could avoid [d] the [a] [n] without [d] the [a] [n].", 
+			"Here is the [a] [n] and it's time [d] the [a] [n] without [d] the [a] [n].", 
+			"Damn, the [a] [n] could not [v] the [a] [n] ! we will be obliged [d] the [a] [n]...", 
+			"All is well on board. The [a] [n] seems to [v] properly. But we should [v] the [a] [n] for more safety.", 
+			"Warning ! The [a] [n] seems [v] dangerously ! We have to [v] the [a] [n] urgently !!!", 
+			"Please [v] the [a] [n] before [d] deliberately.")
+
+def get_gabarit():
+	i=int(len(gabarits)*random.random())
+	return gabarits[i]
+	
+def substitute_verb(prep):
+	i=int(len(verbs)*random.random())
+	v=verbs[i][0]
+	p=verbs[i][1]
+	if p==1:
+		v="%s-%s" % (substitute_prefix(),v)
+	elif p==2:
+		if random.random()<0.2:
+			v="%s-%s" % (substitute_prefix(),v)
+	if prep:
+			v="to %s" % v
+	return v
+
+def substitute_prefix():
+	i=int(len(prefix)*random.random())
+	return prefix[i]
+
+def substitute_adj(genre):
+	i=int(len(adjs)*random.random())
+	return adjs[i][genre]
+
+def substitute_noun(genre):
+	i=int(len(nouns)*random.random())
+	n=nouns[i][0]	# the nouns
+	g=nouns[i][1]	# the genre
+	p=nouns[i][2]	# prefix ?
+	withAdj=False
+	
+	if genre>0 and genre<>g :
+		substitute_noun(genre)
+	else:
+		if p==1:
+			n="%s-%s" % (substitute_prefix(),n)
+		elif p==2:
+			if random.random()<0.2:
+				n="%s-%s" % (substitute_prefix(),n)
+		if withAdj:
+			n="%s %s" % (n,substitute_adj(g))
+		
+	return n
+
+def main():
+	p0=get_gabarit()
+	p1=""
+	
+	pattern=("[v]","[n]","[a]","[d]","[m]","[f]")
+	
+	for w in p0.split():
+		pat=False
+		for p in pattern:
+			if w.find(p)>=0:
+				s=""
+				if p==pattern[0]:
+					s=substitute_verb(False)
+				if p==pattern[1]:
+					s=substitute_noun(0)
+				if p==pattern[2]:
+					s=substitute_adj(0)
+				if p==pattern[3]:
+					s=substitute_verb(True)
+				if p==pattern[4]:
+					s=substitute_noun(1)
+				if p==pattern[5]:
+					s=substitute_noun(2)
+				pat=True
+				w=s
+		if len(p1)==0:
+			p1=w
+		else:
+			p1=" ".join((p1,w))
+
+	print p1		
+	
+if __name__=="__main__":
+	main()
---
+++ fr/CorvoNG.py
@@ -0,0 +1,187 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	Corvophraseur New Generation
+	-----------------------------
+	copyright 2010, Pierre-Alain Dorange
+	
+	Adapted from the AppleScript CorvoX (for Adium/Mac) from Aurelien
+		http://www.adiumxtras.com/index.php?a=xtras&xtra_id=2646
+	Original concept : Luc 'Skywalker' Heinrich
+		Corvophraseur 2 (for MacOS 9)
+
+	BSD Licence
+"""
+
+__author__="Pierre-Alain Dorange (pdorange@mac.com)"
+__date__="6 avril 2010"
+__copyright__="Copyright 2010, Pierre-Alain Dorange"
+__license__="BSD"
+__version__="0.1a"
+
+import random
+
+verbs = ( 	("accéder",2), ("accélérer",2), ("ajuster",2), ("améliorer",2), ("amplifier",2), 
+			("analyser",2), ("annuler",2), ("bouffoner",2), ("bouillir",2), ("calcifier",2), 
+			("compresser",2), ("créer",2), ("décélérer",2), ("décoder",2), ("décrypter",2), 
+			("déstabiliser",2), ("détecter",2), ("diminuer",2), ("dupliquer",2), ("encoder",2), 
+			("encrypter",2), ("fracasser",2), ("induire",2), ("inhiber",2), ("inverser",2), 
+			("ioniser",2), ("moduler",2), ("neutraliser",2), ("optimiser",2), ("phaser",2), 
+			("polariser",2), ("pournifier",2), ("réarranger",2), ("recombiner",2), ("recréer",2), 
+			("rediriger",2), ("relayer",2), ("scanner",2), ("signaler",2), ("stabilisr",2), 
+			("surcharger",2), ("transformer",2), ("trouer",2), ("zombifier",2))
+adjs = (	("à court rayon d'action", "à court rayon d'action", 0), ("à grand rayon d'action", "à grand rayon d'action", 0), 
+			("à haute vitesse", "à haute vitesse", 0), ("adaptatif", "adaptative", 2), ("alternatif", "alternative", 2), 
+			("alvéolé", "alvéolée", 2), ("artificiel", "artificielle", 2), ("atomique", "atomique", 2), 
+			("baleiné", "baleinée", 2), ("ballistique", "ballistique", 2), ("basse vitesse", "basse vitesse", 2), 
+			("binaire", "binaire", 2), ("bouffoné", "bouffonée", 2), ("calorifique", "calorifique", 2), 
+			("carpien", "carpienne", 2), ("centrique", "centrique", 2), ("compressé", "compressée", 2), 
+			("connotatif", "connotative", 2), ("corporel", "corporelle", 2), ("cythérien", "cythérienne", 2), 
+			("dimensionnel", "dimensionnelle", 2), ("directionnel", "directionnelle", 2), ("dirigé", "dirigée", 2), 
+			("dynamique", "dynamique", 2), ("encrypté", "encryptée", 2), ("génique", "génique", 2), 
+			("gildoique", "gildoique", 2), ("gravifique", "gravifique", 2), ("gravitationnel", "gravitationnelle", 2), 
+			("holographique", "holographique", 2), ("instable", "instable", 2), ("interstellaire", "interstellaire", 2), 
+			("ionisé", "ionisée", 2), ("linéaire", "linéaire", 2), ("localisé", "localisée", 2), ("magnétique", "magnétique", 2), 
+			("mécanique", "mécanique", 2), ("microscopique", "microscopique", 2), ("modulaire", "modulaire", 2), 
+			("moléculaire", "moléclaire", 2), ("moncturien", "moncturienne", 2), ("navigationnel", "navigationnelle", 2), 
+			("oblatif", "oblative", 2), ("ossiphazolé", "ossiphazolée", 2), ("parabolique", "parabolique", 2), 
+			("parallele", "parallele", 2), ("phasé", "phasée", 2), ("phasique", "phasique", 2), ("plutonnant", "plutonnante", 2), 
+			("plutonné", "plutonnée", 2), ("pourniflant", "pourniflante", 2), ("quantique", "quantique", 2), 
+			("refroidi", "refroidie", 2), ("réplicatif", "réplicative", 2), ("résistant", "résistante", 2), 
+			("résonnant", "résonnante", 2), ("spatial", "spatiale", 2), ("spinoidal", "spinoidale", 2), ("statique", "statique", 2), 
+			("stellaire", "stellaire", 2), ("temporel", "temporelle", 2), ("trigloidal", "trigloidale", 2), ("valvué", "valvuée",2), 
+			("véloce","véloce",2), ("vibratile","vibratile",2))
+nouns = ( 	("aimant",1,2), ("alignement",1,2), ("amplificateur",1,2), ("anihilateur",1,2), ("capsule",2,2), 
+			("assimilateur",1,2), ("balise",2,2), ("bouclier",1,2), ("bouffon",1,2), ("buffer",1,2), 
+			("calcifrage",1,2), ("capacité",2,2), ("capilectomie",2,2), ("causalité",2,2), ("chambre",2,2), 
+			("champ",1,2), ("champ de force",1,2), ("coeur",1,2), ("conduit",1,2), ("configuration",2,2), 
+			("continuum",1,2), ("convertisseur",1,2), ("corridor",1,2), ("crevasse",2,2), ("cristal",1,2), 
+			("cybergement",1,2), ("déflecteur",1,2), ("désintégrateur",1,2), ("détonateur",1,2), 
+			("diagnostic",1,2), ("disrupteur",1,2), ("distortion",2,2), ("écho",1,2), ("efficience",2,2), 
+			("émission",2,2), ("endiguement",1,2), ("énergie",2,2), ("entité",2,2), ("filament",1,2), 
+			("filtre",1,2), ("flot",1,2), ("flux",1,2), ("force",2,2), ("formation",2,2), ("fragment",1,2), 
+			("fragmenticule",1,2), ("fréquence",2,2), ("gain",1,2), ("générateur",1,2), ("gicleur",1,2), 
+			("glomérule",2,2), ("goniotron",1,2), ("graviton",1,2), ("grille",2,2), ("hologramme",1,2), 
+			("impulsion",2,2), ("incursion",2,2), ("inducteur",1,2), ("inertie",1,2), ("inhibiteur",1,2), 
+			("laplaxmol",1,2), ("matiere",2,2), ("matrice",2,2), ("mécanisme",1,2), ("mine",2,2), 
+			("mitochondrie",2,2), ("moteur",1,2), ("moulinotron",1,2), ("multiplexeur",1,2), ("nacelle",2,2), 
+			("noyau",1,2), ("nuage",1,2), ("ordinateur",1,2), ("papsouille",2,2), ("parallax",1,2), 
+			("particule",2,2), ("plasma",1,2), ("plutonneur",1,2), ("porteuse",2,2), ("poussée",2,2), 
+			("radiation",2,2), ("rayon",1,2), ("réhausseur",1,2), ("relai",1,2), ("rémouleur",1,2), 
+			("réplicateur",1,2), ("réplication",2,2), ("réseau",1,2), ("résonnateur",1,2), ("ressort",1,2), 
+			("rotation",2,2), ("schisme",1,2), ("séquence",2,2), ("signal",1,2), ("signature",2,2), 
+			("singularité",2,2), ("sonde",2,2), ("spouniseur",1,2), ("survolteur",1,2), ("sustentation",2,2), 
+			("syntagme",1,2), ("tachyon",1,2), ("transistor",1,2), ("translateur",1,2), ("transporteur",1,2), 
+			("trigloide",1,2), ("trophoblaste",1,2), ("tropisme",1,2), ("tube",1,2), ("tunnel",1,2), 
+			("turbulence",1,2), ("vagissement",1,2), ("vague",2,2), ("vibration",2,2), ("vide",1,2), ("vortex",1,2))
+prefix = ( 	"aéro","ana","anti","auto","bi","bulbo","capillo","crypto","extra","hepta","hétéro",
+			"homo","méta","micro","morpho","morvo","multi","néo","non","nucléo","octo","penta",
+			"poly","proto","pseudo","quadri","rétro","servo","spiro","sub","sur","thermo","theta",
+			"trans","tri","turbo" )
+gabarits = ("Si nous pouvons [v] [n] [a], nous devrions [v] [n] [a] et [v] [n]", 
+			"Capitaine, je ne peux pas [v] [n] parce que [n] [a] est sur le point [d] [n] [a] !",
+			"[v] [n] [a] est illogique, puisque [n] [a] va [v] [n] [a].", 
+			"Il est possible que [n] [a] puisse [v] [n] [a], mais seulement si nous pouvons [v] [n] [a] et [v] [n] [a] !", 
+			"Pas de panique ! [v] [n] [a] ne nous empêche pas [d] [n] [a] ni même [d] [n] [a].", 
+			"Voici [n] [a] dont il est temps [d] [n] [a] sans oublier [d] [n] [a].", 
+			"Damned, [n] [a] ne peut pas [v] [n] [a] ! Nous allons être obligé [d] [n] [a]...", 
+			"Tout va bien a bord. [n] [a] semble [v] correctement. Mais nous devrions [v] [n] [a] pour plus de sécurité.", 
+			"Alerte ! [n] [a] semble [v] dangereusement ! Il faut [v] [n] [a] d'urgence !!!", 
+			"Veuillez [v] [n] [a] avant [d] sciemment.")
+
+def get_gabarit():
+	i=int(len(gabarits)*random.random())
+	return gabarits[i]
+	
+def substitute_verb(prep):
+	i=int(len(verbs)*random.random())
+	v=verbs[i][0]
+	p=verbs[i][1]
+	if p==1:
+		v="%s-%s" % (substitute_prefix(),v)
+	elif p==2:
+		if random.random()<0.2:
+			v="%s-%s" % (substitute_prefix(),v)
+	if prep:
+		if check_voyelle(v):
+			v="d'%s" % v
+		else:
+			v="de %s" % v
+	return v
+
+def substitute_prefix():
+	i=int(len(prefix)*random.random())
+	return prefix[i]
+
+def substitute_adj(genre):
+	i=int(len(adjs)*random.random())
+	return adjs[i][genre]
+
+def substitute_noun(genre):
+	i=int(len(nouns)*random.random())
+	n=nouns[i][0]	# the nouns
+	g=nouns[i][1]	# the genre
+	p=nouns[i][2]	# prefix ?
+	withAdj=False
+	
+	if genre>0 and genre<>g :
+		substitute_noun(genre)
+	else:
+		if p==1:
+			n="%s-%s" % (substitute_prefix(),n)
+		elif p==2:
+			if random.random()<0.2:
+				n="%s-%s" % (substitute_prefix(),n)
+		if withAdj:
+			n="%s %s" % (n,substitute_adj(g))
+	if check_voyelle(n):
+		n="l'%s" % n
+	elif g==1:
+		n="le %s" % n
+	else:
+		n="la %s" % n
+		
+	return n
+
+def check_voyelle(v):
+	s="aeiouyéh"
+	return (s.find(v[0])>=00)
+
+def main():
+	print "Corvophraseur (New Generation)",__version__
+	print "---------------------------------------------"
+
+	p0=get_gabarit()
+	p1=""
+	
+	pattern=("[v]","[n]","[a]","[d]","[m]","[f]")
+	
+	for w in p0.split():
+		pat=False
+		for p in pattern:
+			if w.find(p)>=0:
+				s=""
+				if p==pattern[0]:
+					s=substitute_verb(False)
+				if p==pattern[1]:
+					s=substitute_noun(0)
+				if p==pattern[2]:
+					s=substitute_adj(0)
+				if p==pattern[3]:
+					s=substitute_verb(True)
+				if p==pattern[4]:
+					s=substitute_noun(1)
+				if p==pattern[5]:
+					s=substitute_noun(2)
+				pat=True
+				w=s
+		if len(p1)==0:
+			p1=w
+		else:
+			p1=" ".join((p1,w))
+
+	print p1		
+	
+if __name__=="__main__":
+	main()
